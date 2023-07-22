f05315b9f90522607ac941e68b475928d81321d9
Hojiakbar Zokirov <hojiakbar312@gmail.com>
2023-07-23 01:48:48 +0500

    CODE RUB:Add Route

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,27 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return "Hi, about page";
+});
+
+Route::get('/contact', function () {
+    return "Hi, contact page";
+});
+
+Route::get('/post/{id}', function ($id) {
+
+    return "This is post number". $id;
+});
+
+Route::get('admin/posts/example', array('as'=>'admin.home' ,function(){
+    $url =route('admin.home');
+
+    return "This url is ". $url;
+}));
+
