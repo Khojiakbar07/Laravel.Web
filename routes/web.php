b31799379ab013738488e69e276c8f8dbdf60fef
Hojiakbar Zokirov <hojiakbar312@gmail.com>
2023-07-26 15:59:50 +0500

    CODE RUB:Create Crud

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\PostController;
 
 Route::get('/' , [PageController::class, 'main']);
 Route::get('/index.html' , [PageController::class, 'main']);
@@ -9,9 +10,12 @@ Route::get('/about.html' , [PageController::class, 'about']);
 Route::get('/service.html' , [PageController::class, 'service']);
 Route::get('/price.html' , [PageController::class, 'price']);
 
+Route::resource('posts', PostController::class); 
 
-Route::get();
-
-
-
-
+// Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+// Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+// Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+// Route::post('/posts/create', [PostController::class, 'store'])->name('posts.store');
+// Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+// Route::put('/posts/{post}/edit', [PostController::class, 'update'])->name('posts.update');
+// Route::delete('/posts/{post}/delete', [PostController::class, 'delete'])->name('posts.delete');
