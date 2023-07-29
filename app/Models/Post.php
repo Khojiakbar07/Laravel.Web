e6d648f3c15a02ec7b81fb24386df4ae4283bf03
Hojiakbar Zokirov <hojiakbar312@gmail.com>
2023-07-29 15:27:28 +0500

    CODE RUB:Blade ->WIP

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Post extends Model
 {
-    use HasFactory;
+    public function phone(): HasOne
+    {
+        return $this->hasOne(Phone::class);
+    }
 }
